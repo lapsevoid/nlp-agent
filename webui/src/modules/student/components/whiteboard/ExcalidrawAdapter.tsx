720936bdf2a0f527ab6a9233b6df534341b21aa0
liunor <a13550112138@outlook.com>
2026-09-07 10:16:39 +0800

    fix(whiteboard): remove deprecated welcome option

--- webui/src/modules/student/components/whiteboard/ExcalidrawAdapter.tsx
+++ webui/src/modules/student/components/whiteboard/ExcalidrawAdapter.tsx
@@ -28,7 +28,6 @@ export function ExcalidrawAdapter({ initialScene, onChange }: {
       langCode="zh-CN"
       onChange={(elements, appState, files) => onChange({ elements, appState, files })}
       aiEnabled={false}
-      UIOptions={{ welcomeScreen: false }}
       validateEmbeddable={() => false}
     />
   </section>;
